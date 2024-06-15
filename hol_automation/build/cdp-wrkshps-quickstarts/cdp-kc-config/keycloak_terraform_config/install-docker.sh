e087861459b7836a9f30d852b4ae0d13ecdb4274
Kuldeep Sahu <ksahu@cloudera.com>
2024-06-15 06:39:46 +0000

    Added CDP AWS Hol Automation Code

---
+++ hol_automation/build/cdp-wrkshps-quickstarts/cdp-kc-config/keycloak_terraform_config/install-docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+apt-get update
+#apt-get -y upgrade
+apt-get -y install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt-get -y install ansible
+
+# Add Docker's official GPG key:
+apt-get -y install apt-transport-https ca-certificates curl 
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# Start and enable Docker service
+systemctl start docker
+systemctl enable docker
+docker run -d -p 80:8080 --name=keycloak -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=${keycloak_admin_password} keycloak/keycloak start-dev >> /tmp/kc_init.log
+sleep 40
+docker exec keycloak /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password ${keycloak_admin_password} >> /tmp/kc_init.log
+sleep 5
+docker exec keycloak /opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE --server http://localhost:8080 --realm master --user admin --password ${keycloak_admin_password} >> /tmp/kc_init.log
---
+++ hol_automation/build/cdp-wrkshps-quickstarts/cdp-kc-config/keycloak_terraform_config/variables.tf
@@ -0,0 +1,53 @@
+variable instance_name {
+  description = "Name Of the EC2 instance"
+  type = string
+  default = "keycloak-server"
+}
+
+variable "aws_region" {
+  description = "AWS Region In which Resources Will Be Deployed"
+  type = string
+  default = "ap-south-1"  
+}
+
+variable "instance_type" {
+  description = "Instance Type "
+  type = string
+  default = "t2.xlarge"  
+}
+
+variable "instance_keypair" {
+  description = "SSH KeyPair For EC2 Instance"
+  type = string
+  default = "puneetjoshipsedaily"  
+}
+
+variable "kc_security_group" {
+  description = "Security Group Name For EC2"
+  type = string
+  default = "hol-default-sg"
+}
+variable "local_ip" {
+  description = "IPV4 Address User's Local Machine"
+  type = string
+  default = "0.0.0.0/0"
+}
+variable "keycloak_admin_password" {
+  description = "admin password for keycloak"
+  type = string
+}
+variable "amis" {
+  type = map(string)
+  default = {
+    "us-east-2" = "ami-0f30a9c3a48f3fa79"
+    "us-east-1" = "ami-0e001c9271cf7f3b9"
+    "us-west-1" = "ami-036cafe742923b3d9"
+    "us-west-2" = "ami-03c983f9003cb9cd1"
+    "ap-south-1" = "ami-05e00961530ae1b55"
+    "ap-southeast-1" = "ami-0be48b687295f8bd6"
+    "ap-southeast-2" = "ami-01ed8ade75d4eee2f"
+    "eu-central-1" = "ami-026c3177c9bd54288"
+    "eu-west-1" = "ami-0607a9783dd204cae"
+    "eu-west-2" = "ami-09627c82937ccdd6d"
+  }
+}
