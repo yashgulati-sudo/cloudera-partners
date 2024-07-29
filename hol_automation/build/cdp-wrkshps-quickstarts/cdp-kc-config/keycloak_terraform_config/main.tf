6ddc50f1fe1d35aaa53281fddecdb2816d04320b
Kuldeep Sahu <ksahu@cloudera.com>
2024-07-29 12:37:40 +0530

    Dockerfile update: TF version to 1.9.3

--- hol_automation/build/cdp-wrkshps-quickstarts/cdp-kc-config/keycloak_terraform_config/main.tf
+++ hol_automation/build/cdp-wrkshps-quickstarts/cdp-kc-config/keycloak_terraform_config/main.tf
@@ -1,10 +1,11 @@
 # Terraform Block
 terraform {
-  required_version = "1.4.6"
+  required_version = ">= 1.4.6"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.65.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.65.0"
     }
   }
 }
