2aa042332bea32d7e79044f9248d53ae67966e1b
Avanish Tiwari <aktiwari@cloudera.com>
2025-05-07 18:43:51 +0530

    Updated readme instructions and yaml files

--- K8SOperators/parameters.sh
+++
@@ -1,6 +0,0 @@
-Cloudera_username=$Cloudera_username
-Cloudera_password=$Cloudera_password
-cfm_operator_version=2.9.0-b96
-csa_operator_version=1.2.0-b27
-strimzi-kafka-operator=1.3.0-b52
-ldapadmin_password=${ldapadmin_password}
