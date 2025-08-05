8709be8051ba19acc6331398d3db34ca7149e55d
Yash Gulati <ygulati@cloudera.com>
2025-08-05 22:41:46 +0530

    Replaced DataServicesLabs folder with submodule

--- DataServicesLabs/ClouderaMachineLearning/SmartChatbot-GenAI/CML_llm-hol-main/0_install_prerequisites/download_reqs_set_vars.py
+++
@@ -1,3 +0,0 @@
-# !pip install --no-cache-dir -r 0_install_prerequisites/requirements.txt
-!pip install --no-cache-dir -r 0_install_prerequisites/requirements.txt
-!pip install pinecone-client==3.2.2 --upgrade
